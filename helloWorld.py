95b60eccf899045da729d6cf2570b3a3c6dae871
Tim Cordoncd <timcordon1@hotmail.com>
2024-07-18 15:14:47 +0100

    Notes added

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,7 @@
+# As instructed by task
 print("Git is awesome!")
 
+# Request user enter something
 input_string = input("\nPlease enter something intersting ")
-
+# Print string
 print(input_string)
