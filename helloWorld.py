0a7e1c223550b84426f1e10b59657d9edcbb4652
Tim Cordoncd <timcordon1@hotmail.com>
2024-07-18 14:35:22 +0100

    notes added

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,7 @@
+# Entered as requested by compulsory task
 print("Git is awesome!")
 
+# Request user enter info
 input_string = input("\nPlease enter something intersting ")
-
+# Print string
 print(input_string)
